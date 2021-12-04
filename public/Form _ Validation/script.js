08ab72f6feebfc3cbef8357c3e39adfc1cb8a454
shahayush480 <79455386+shahayush480@users.noreply.github.com>
2021-12-04 10:24:33 +0530

    done

--- public/Form _ Validation/script.js
+++ public/Form _ Validation/script.js
@@ -1,31 +1,20 @@
 
 let create = document.querySelector("#create");
-let errors = [
-    "Ensure the username is at least 6 characters long",
-    "Ensure the password is at least 10 characters long",
-    "Ensure the password and confirmation password match",
-    "Ensure the terms checkbox is checked"
-]
+let errors = []
 
 let errortag = document.querySelector(".errors");
 
-let errorlist = document.querySelector(".errors-list")
+let errorlist = document.querySelector(".errors-list");
+
 
 function clearErrors() {
-    
-errorlist.innerHTML = "";
-errortag.classList.remove("show")
+    errorlist.innerHTML = "";
+    errortag.classList.remove("show")
 
 
 }
 
 
-function showErrors(i) {
-    let createli = document.createElement("li")
-    errortag.classList.add("show")
-    errorlist.appendChild(createli)
-    createli.innerHTML = errors[i];
-}
 
 
 create.addEventListener('click', function (a) {
@@ -35,7 +24,7 @@ create.addEventListener('click', function (a) {
 
     if (username.length <= 6) {
         console.log("ayush");
-        showErrors(0);
+        errors.push("Ensure the username is at least 6 characters long")
         a.preventDefault();
     }
 
@@ -43,21 +32,35 @@ create.addEventListener('click', function (a) {
     let cpass = document.querySelector("#password-confirmation").value;
     if (pass.length < 9) {
         console.log("ayush");
-        showErrors(1);
+        errors.push("Ensure the password is at least 10 characters long")
         a.preventDefault();
     }
     if (cpass !== pass) {
         console.log("ayush");
-        showErrors(2);
+        errors.push("Ensure the password and confirmation password match")
         a.preventDefault();
     }
     let terms = document.querySelector("#terms").checked;
     if (terms == false) {
         console.log("ayush");
-        showErrors(3);
+        errors.push("Ensure the terms checkbox is checked")
         a.preventDefault();
     }
-
+    showErrors()
 
 })
 
+
+
+function showErrors() {
+    let createli = document.createElement("li")
+    errortag.classList.add("show")
+    errorlist.appendChild(createli)
+    createli.innerHTML = errors;
+    console.log(errors)
+    for (let i = 0; i < errors.length; i++) {
+        console.log("hello")
+        errorlist.innerHTML = "<li>" + errors[i] + "</li>";
+        
+    }
+}
